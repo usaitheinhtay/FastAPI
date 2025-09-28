767ffe493222d6c4135fe3409824603ad37b1897
saitheinhtay <saitheinhtay2025@gmail.com>
2025-09-29 04:41:38 +0700

    user section

---
+++ noteapp/app/crud/user.py
@@ -0,0 +1,19 @@
+from app.db import db
+from app.models.user import UserRegister
+
+user_collection = db["users"]
+
+
+async  def register_user(user: UserRegister):
+    user_data = user.model_dump()
+    result = await user_collection.insert_one(user_data)
+    return UserRegister(id=str(result.inserted_id), **user_data)
+
+
+async def get_user_by_email(email: str):
+    user = await user_collection.find_one({"email": email})
+    if user :
+        return UserRegister(id=str(user["_id"]), **user)
+    return None
+
+
---
+++ noteapp/app/models/user.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+
+class UserRegister(BaseModel):
+    name: str
+    email: str
+    password: str
+    created_at: Optional[datetime] = datetime.now()
+    updated_at: Optional[datetime] = datetime.now()
+
+class UserLogin(BaseModel):
+    name: str
+    password: str
+
+
